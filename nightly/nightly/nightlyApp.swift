1fe712d4bd6e55edae29206a9b01751aa10f136c
Julia Teleki <julia@teleki.net>
2025-08-22 16:04:08 -0500

    added xcode project

---
+++ nightly/nightly/nightlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  nightlyApp.swift
+//  nightly
+//
+//  Created by Julia Teleki on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct nightlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nightly/nightlyTests/nightlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  nightlyTests.swift
+//  nightlyTests
+//
+//  Created by Julia Teleki on 8/22/25.
+//
+
+import Testing
+@testable import nightly
+
+struct nightlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
